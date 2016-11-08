e1a64aa7b7b58a310372be414fc915a699681d83
Sebastian Pina Otey <spinaotey@gmail.com>
2016-11-08 10:55:12 +0100

    Change strtok by own function strtok_single

--- mkGr.c
+++ mkGr.c
@@ -5,6 +5,46 @@
 #include <stdlib.h>
 #include <assert.h>
 
+
+
+/*  strtok_single
+
+    strtok manual implementation where zero string elements are
+    taken into account.
+
+    Variables:
+        -str = Input string to find next delims.
+        -delims = string to separate the input.
+    
+    Return value:
+        Pointer to next start of word, modifiying the delims
+        character by '\0' character.
+ */
+char * strtok_single (char * str, char const * delims){
+
+    static char  * src = NULL;
+    char  *  p,  * ret = 0;
+
+    if (str != NULL)
+        src = str;
+
+    if (src == NULL)
+        return NULL;
+
+    if ((p = strpbrk (src, delims)) != NULL) {
+        *p  = 0;
+        ret = src;
+        src = ++p;
+
+    } else if (*src) {
+        ret = src;
+        src = NULL;
+    }
+
+    return ret;
+}
+
+
 /*  sep_line
 
     Given an input string (line) and a string of characters for separation
@@ -16,7 +56,7 @@
 
     Variables:
         -line = Input string to be separated.
-        -separatos = Characters to separate the string.
+        -separators = Characters to separate the string.
         -elements = Output vector of strings.
         -n = number of elements obtained.
  
@@ -28,14 +68,14 @@ void sep_line(char *line, char *separators, char ***elements, uint32_t *n){
     *n = 0;
     *elements = (char **) malloc(sizeof(char *)); assert(*elements);
 
-    token = strtok(line,separators);
+    token = strtok_single(line,separators);
 
     while(token != NULL){
         (*n)++;
         *elements = (char **) realloc(*elements,sizeof(char *)*(*n));assert(*elements);
         (*elements)[(*n)-1]  = (char *) malloc(sizeof(char)*strlen(token)+1);assert((*elements)[(*n)-1]);
         strcpy((*elements)[(*n)-1],token);
-        token = strtok(NULL,separators);
+        token = strtok_single(NULL,separators);
     }
 
 }
--- mkGr.h
+++ mkGr.h
@@ -1,6 +1,22 @@
 #pragma once
 #include <inttypes.h>
 
+/*  strtok_single
+
+    strtok manual implementation where zero string elements are
+    taken into account.
+
+    Variables:
+        -str = Input string to find next delims.
+        -delims = string to separate the input.
+    
+    Return value:
+        Pointer to next start of word, modifiying the delims
+        character by '\0' character.
+ */
+char * strtok_single (char * str, char const * delims);
+
+
 /*  sep_line
 
     Given an input string (line) and a string of characters for separation
