474fb18480f8c50c592b2751e2729bc40d1cbf7c
Sebastian Pina Otey <spinaotey@gmail.com>
2017-01-20 11:27:51 +0100

    Add execution file

---
+++ Makefile
@@ -0,0 +1,42 @@
+COMPILER        =       gcc
+CFLAGS          =       -Ofast
+LFLAGS          =       -lm
+OBJECTS         =       main.o epi.o 
+OBJECTSTEST		=		test.o mkGr.o
+INCLUDES        =       mkGr.h myFunctions.h aStar.h
+
+main:           main.o mkGr.o aStar.o myFunctions.o
+		$(COMPILER) $(CFLAGS) -o main main.o mkGr.o aStar.o myFunctions.o $(LFLAGS)
+
+main.o: main.c $(INCLUDES)
+		$(COMPILER) $(CFLGS) -c main.c $(LFLAGS)
+
+mkGr.o:			mkGr.c $(INCLUDES)
+		$(COMPILER) $(CFLAGS) -c mkGr.c $(LFLAGS)
+
+aStar.o: aStar.c $(INCLUDES)
+		$(COMPILER) $(CFLGS) -c aStar.c $(LFLAGS)
+
+
+myFunctions.o: myFunctions.c $(INCLUDES)
+		$(COMPILER) $(CFLGS) -c myFunctions.c $(LFLAGS)
+
+test:			$(OBJECTSTEST)
+		$(COMPILER) $(CFLAGS) -o test $(OBJECTSTEST) $(LFLAGS)
+runTest:		test
+		perf stat ./test spain.csv bla \| 3 23895681 1417363 79858
+
+test.o:		$(INCLUDES) test.c
+		$(COMPILER) $(CFLAGS) -c test.c $(LFLAGS)
+
+runTest:		main
+		perf stat ./main bla 240949599 195977239
+
+clean:
+		rm -f *.o *~
+
+realclean:	clean
+		rm -f main
+
+tclean: clean
+		rm -f test
---
+++ main.c
@@ -0,0 +1,91 @@
+#include "aStar.h"
+#include "mkGr.h"
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    
+    uint64_t nNodes,nSucc,nameLen;
+    uint64_t startId, targetId, startNode, targetNode;
+    uint64_t aux1,aux2,i;
+    uint64_t *successors;
+    char *nodeNames;
+    AStarStatus_t *status; 
+    node_t *nodes;
+    FILE *binIn, *solutionF;
+    
+    /* INPUT */
+    if (argc < 4 ||
+        sscanf(argv[2],"%"SCNi64, &startId)!=1 ||
+        sscanf(argv[3],"%"SCNi64, &targetId)!=1 
+       ) {
+          fprintf(stderr,"%s filename startId targetId\n",argv[0]);
+          return 1;
+    }
+
+    /* READ GRAPH FROM BINARY FILE */
+    binIn = fopen(argv[1],"r");
+    fread(&nNodes,sizeof(uint64_t),1,binIn);
+    fread(&nSucc,sizeof(uint64_t),1,binIn);
+    fread(&nameLen,sizeof(uint64_t),1,binIn);
+
+    nodes = malloc(sizeof(node_t)*nNodes); assert(nodes);
+    successors = malloc(sizeof(uint64_t)*nSucc); assert(successors);
+    nodeNames = malloc(sizeof(char)*nameLen); assert(nodeNames);
+
+    fread(nodes,sizeof(node_t),nNodes,binIn);
+    fread(successors,sizeof(uint64_t),nSucc,binIn);
+    fread(nodeNames,sizeof(char),nameLen,binIn);
+
+    fclose(binIn);
+    
+    aux1 = aux2 = 0;
+    for(i=0;i<nNodes;i++){
+        if(nodes[i].nsucc != 0){
+            nodes[i].successors = successors+aux1;
+            aux1 += nodes[i].nsucc;
+        }
+        nodes[i].name = nodeNames+aux2;
+        aux2 += 1+strlen(nodeNames+aux2);
+    }
+    
+    /* Find initial nodes */
+    startNode = findNode(nodes,nNodes,startId);
+    if(startNode == -1){
+        fprintf(stderr,"ERROR: Start node not found in graph.\n");
+        return -1;
+    }else
+        fprintf(stderr,"Starting node found in position %"PRIu64".\n",startNode);
+    targetNode = findNode(nodes,nNodes,targetId);
+    if(targetNode == -1){
+        fprintf(stderr,"ERROR: Target node not found in graph.\n");
+        return -2;
+    }else
+        fprintf(stderr,"Target node found in position %"PRIu64".\n",targetNode);
+    /* Initiate status */
+    status = malloc(sizeof(AStarStatus_t)*nNodes); assert(status);
+    for(i=0; i<nNodes;i++)
+        status[i].whq = NONE;
+    if(aStarAlgorithm(nodes,status,nNodes,startNode,targetNode) == 0){
+        fprintf(stderr,"Solution found, with distance %lf\n",status[targetNode].g);
+    }else{
+        fprintf(stderr,"ERROR: No path was found\n");
+    }
+
+    solutionF = fopen("solution.dat","w");
+    aux1 = targetNode;
+    while(aux1 != startNode){
+        fprintf(solutionF,"Node id: %10"PRIu64" | Distance: %10.2lf | Name: %s\n",
+                nodes[aux1].id,status[aux1].g,nodes[aux1].name);
+        aux1 = status[aux1].parent;
+    }
+    fprintf(solutionF,"Node id: %10"PRIu64" | Distance: %10.2lf | Name: %s\n",
+            nodes[aux1].id,status[aux1].g,nodes[aux1].name);
+    fclose(solutionF);
+    
+    free(nodes); free(successors); free(nodeNames); free(status);
+    return 0;
+}
